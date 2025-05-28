422247487c26ae44241ce25a98278bc1ce51fb84
gabriel <firminogabriel713@gmail.com>
2025-05-28 06:42:09 -0300

    alterações tela-1

---
+++ src/pages/RequestPage.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReservationForm from "../components/ReservationForm";
+
+const RequestPage = () => (
+  <div>
+    <h2>Nova Solicitação</h2>
+    <ReservationForm />
+  </div>
+);
+
+export default RequestPage;
