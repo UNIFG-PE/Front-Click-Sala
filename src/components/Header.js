bc3ccee98c659ecbc77884434544f0cd2e1d4fea
luana0713 <166567157+luana0713@users.noreply.github.com>
2025-06-05 00:20:00 -0300

    feat: add logout icon with confirmation modal including Yes and No buttons

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';  // IMPORTAR useState
+import { FiLogOut } from 'react-icons/fi';
+import Logout from './logout'; // Importa o componente modal Logout (arquivo Logout.js)
 import './Header.css';
 
 function Header() {
+  const [showLogoutModal, setShowLogoutModal] = useState(false);
+  const handleLogoutClick = () => setShowLogoutModal(true);
+
+  const handleConfirmLogout = () => {
+    setShowLogoutModal(false);
+    console.log('usurio confirmou o logout');
+  };
+
+  const handleCancelLogout = () => {
+    setShowLogoutModal(false);
+    console.log('usuario cancelou o ogout');
+  };
+
   return (
     <header className="header">
       <div className="logo">
@@ -13,9 +28,19 @@ function Header() {
           <li><a href="/">Home</a></li>
           <li><a href="/reservas">Minhas Reservas</a></li>
           <li><a href="/salas">Salas</a></li>
-           <li><a href="/suporte">Suporte</a></li>
+          <li><a href="/suporte">Suporte</a></li>
+          <li>
+            <span className="nav-link" onClick={handleLogoutClick} style={{ cursor: 'pointer' }}>
+              <FiLogOut size={23}/>
+            </span>
+          </li>
         </ul>
       </nav>
+
+      {}
+      {showLogoutModal && (
+        <Logout onConfirm={handleConfirmLogout} onCancel={handleCancelLogout} />
+      )}
     </header>
   );
 }
