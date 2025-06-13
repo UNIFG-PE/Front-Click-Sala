158a824d6a3722f0de88522b37711207aeed2d2f
mariane <mariane.assis.bezerra@gmail.com>
2025-06-12 21:02:30 -0300

    create nos recursos

--- src/pages/manageresource/CreateResource.js
+++ src/pages/manageresource/CreateResource.js
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React, { useState } from "react";   
+//push teste
 import CreateResource,{useState} from "CreateResource.css"
 
 function CreateResource({ onSuccess }) {
