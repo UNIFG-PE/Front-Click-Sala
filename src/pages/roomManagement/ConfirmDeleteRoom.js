308c7387cbcae046be6c667f97bcd0db3a6e650f
Stefany Marinho <stefanymarinho@MacBook-Air-de-Stefany.local>
2025-06-23 13:54:30 -0300

    fix:runtime errors for correct functionality after merge from gestao_de_salas

--- src/pages/roomManagement/ConfirmDeleteRoom.js
+++ src/pages/roomManagement/ConfirmDeleteRoom.js
@@ -4,9 +4,9 @@ import "./CreateRoom.css";
 function ConfirmDeleteRoom({ onClose, onConfirm, roomName }) {
   return (
     <div className="modal-overlay">
-      <div className="modal-content" style={{maxWidth: 350, textAlign: "center"}}/>
-        <h2 style={{marginBottom: 12}}>Confirmar Exclusão </h2>
-        <div style={{marginBottom: 18}}/>
+      <div className="modal-content" style={{ maxWidth: 350, textAlign: "center" }}>
+        <h2 style={{ marginBottom: 12 }}>Confirmar Exclusão</h2>
+        <div style={{ marginBottom: 18 }}>
           Tem certeza que deseja excluir a sala <b>{roomName}</b>?
         </div>
         <div className="modal-actions">
